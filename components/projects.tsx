c322b8da776fd3e998c6e8a015688d855d9c6f20
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-12-11 20:39:11 +0530

    final

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
-import ProjectCard from "./project";
+import ProjectCard from "./project-card";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
